2db3bc5b431dda10d3ac9915d9993199e187231c
ojinikim <ojinikim@gmail.com>
2024-06-10 19:00:47 +0900

    fix :: missing-asset-registry-path 에러 해결

--- metro.config.js
+++ metro.config.js
@@ -1,16 +1,25 @@
-import {getDefaultConfig, mergeConfig} from '@react-native/metro-config';
+/* Metro configuration for React Native
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
 
 const defaultConfig = getDefaultConfig(__dirname);
-const {assetExts, sourceExts} = defaultConfig.resolver;
 
-/**
- * Metro configuration
- * https://reactnative.dev/docs/metro
- *
- * @type {import('metro-config').MetroConfig}
- */
+const {
+  resolver: {sourceExts, assetExts},
+} = getDefaultConfig(__dirname);
+
 const config = {
   transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: true,
+      },
+    }),
     babelTransformerPath: require.resolve('react-native-svg-transformer'),
   },
   resolver: {
@@ -18,71 +27,72 @@ const config = {
     sourceExts: [...sourceExts, 'svg'],
   },
 };
-export default mergeConfig(getDefaultConfig(__dirname), config);
 
-module.exports = {
-  resolver: {
-    assetExts: [
-      'bin',
-      'svg',
-      'png',
-      'jpg',
-      'jpeg',
-      'gif',
-      'webp',
-      'bmp',
-      'tiff',
-      'ico',
-      'ttf',
-      'otf',
-      'woff',
-      'woff2',
-      'mp4',
-      'mpg',
-      'mpeg',
-      'm4v',
-      'mov',
-      'mkv',
-      'webm',
-      'avi',
-      'ogv',
-      '3gp',
-      '3g2',
-      'ts',
-      'srt',
-      'vtt',
-      'wav',
-      'mp3',
-      'ogg',
-      'm4a',
-      'aac',
-      'flac',
-      'amr',
-      'pdf',
-      'zip',
-      'gz',
-      'bz2',
-      '7z',
-      'dmg',
-      'iso',
-      'xz',
-      'dll',
-      'exe',
-      'msi',
-      'app',
-      'deb',
-      'rpm',
-      'bin',
-      'sh',
-      'tar',
-      'tgz',
-      'bz2',
-      '7z',
-      'apk',
-      'aar',
-      'aar',
-      'aab',
-    ],
-    sourceExts: ['jsx', 'js', 'ts', 'tsx', 'json'],
-  },
-};
+module.exports = mergeConfig(defaultConfig, config);
+
+// module.exports = {
+//   resolver: {
+//     assetExts: [
+//       'bin',
+//       'svg',
+//       'png',
+//       'jpg',
+//       'jpeg',
+//       'gif',
+//       'webp',
+//       'bmp',
+//       'tiff',
+//       'ico',
+//       'ttf',
+//       'otf',
+//       'woff',
+//       'woff2',
+//       'mp4',
+//       'mpg',
+//       'mpeg',
+//       'm4v',
+//       'mov',
+//       'mkv',
+//       'webm',
+//       'avi',
+//       'ogv',
+//       '3gp',
+//       '3g2',
+//       'ts',
+//       'srt',
+//       'vtt',
+//       'wav',
+//       'mp3',
+//       'ogg',
+//       'm4a',
+//       'aac',
+//       'flac',
+//       'amr',
+//       'pdf',
+//       'zip',
+//       'gz',
+//       'bz2',
+//       '7z',
+//       'dmg',
+//       'iso',
+//       'xz',
+//       'dll',
+//       'exe',
+//       'msi',
+//       'app',
+//       'deb',
+//       'rpm',
+//       'bin',
+//       'sh',
+//       'tar',
+//       'tgz',
+//       'bz2',
+//       '7z',
+//       'apk',
+//       'aar',
+//       'aar',
+//       'aab',
+//     ],
+//     sourceExts: ['jsx', 'js', 'ts', 'tsx', 'json'],
+//   },
+// };
