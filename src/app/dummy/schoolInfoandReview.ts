4d7aa1d675cb9b8b9bffc787b5b6390c2827299b
ojinikim <ojinikim@gmail.com>
2024-06-12 20:29:50 +0900

    dummy :: 메인 학교리뷰 컴포넌트 사용 더미값

---
+++ src/app/dummy/schoolInfoandReview.ts
@@ -0,0 +1,11 @@
+export const schoolInfoandReview = [
+  {
+    schoolName: '충남대학교',
+    location: '대전 유성구 궁동',
+    name: '김어진',
+    star: 4,
+    review:
+      '시설도 좋고 학교가 너무 넓어서 다리가 아프다는 게 단점이지 나머지는 나쁘지 않습니다! 학교 축제 때 블랙핑크 불',
+  },
+  {},
+];
