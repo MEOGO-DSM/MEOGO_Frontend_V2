a30a69696e628422fcc8ddd0f3f0c4856988ffb0
ojinikim <ojinikim@gmail.com>
2024-06-12 20:21:47 +0900

    add :: svg 추가

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -7,3 +7,14 @@ export * from './Warn';
 export * from './Search';
 export * from './Arrow_Right';
 export * from './Logo_Img';
+<<<<<<< Updated upstream
+=======
+<<<<<<< Updated upstream
+=======
+export * from './Bell';
+export * from './Star';
+export * from './Bookmark';
+export * from './Heart';
+export * from './Menu';
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
