b20b4c8d1226e7955a0c98ca57b572c60af304ca
ojinikim <ojinikim@gmail.com>
2024-06-12 20:16:25 +0900

    dummy :: 학교 검색 페이지 학교리스트 더미값 배열

---
+++ src/app/dummy/schoolList.ts
@@ -0,0 +1,12 @@
+export const schoolList = [
+  {name: '대구대덕초등학교', location: '대구 남구'},
+  {name: '대덕초등학교', location: '대전 유성구 궁동'},
+  {name: '용인대덕초등학교', location: '경인 용인시 수지구'},
+  {name: '대덕소프트웨어마이스터고등학교', location: '충남 당진시'},
+  {name: '대덕중학교', location: '전남 장흥군'},
+  {name: '대구대덕초등학교', location: '대구 남구'},
+  {name: '대덕초등학교', location: '대전 유성구 궁동'},
+  {name: '용인대덕초등학교', location: '경인 용인시 수지구'},
+  {name: '대덕소프트웨어마이스터고등학교', location: '충남 당진시'},
+  {name: '대덕중학교', location: '전남 장흥군'},
+];
