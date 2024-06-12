a1992a6c940f598ec74b16d3aa0eb71f609d4bd5
ojinikim <ojinikim@gmail.com>
2024-06-12 20:07:42 +0900

    add :: 회원가입 아이디, 패스워드 설정 퍼블리싱

---
+++ src/app/signup/IdAndPassword.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import TopBar from '../../components/TopBar';
+import {Arrow_Left} from '../../assets';
+import {styled} from 'styled-components/native';
+import Input from '../../components/Input';
+import {color} from '../../styles/color';
+import Button from '../../components/Button';
+import {useNavigation} from '@react-navigation/native';
+import {TouchableOpacity} from 'react-native';
+
+function IdAndPassword() {
+  const navigation = useNavigation();
+  return (
+    <Container>
+      <TopBar
+        text="회원가입"
+        leftIcon={
+          <TouchableOpacity onPress={() => navigation.navigate('Login')}>
+            <Arrow_Left />
+          </TouchableOpacity>
+        }
+      />
+      <InputBox>
+        <Input autoFocus title="아이디" placeholder="최소 5자, 최대 15자" />
+        <Input
+          password
+          title="비밀번호"
+          placeholder="특수문자 1자 이상, 최대 20자"
+        />
+        <Input password title="비밀번호 확인" placeholder="비밀번호" />
+      </InputBox>
+      <Button text="다음" onPress={() => navigation.navigate('Name')} />
+    </Container>
+  );
+}
+
+export default IdAndPassword;
+
+const Container = styled.View`
+  flex: 1;
+  padding: 80px 20px 24px;
+  background-color: ${color.white};
+  justify-content: space-between;
+`;
+
+const InputBox = styled.View`
+  gap: 12px;
+  width: 100%;
+`;
