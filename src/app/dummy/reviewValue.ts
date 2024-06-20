9cdb9bd50412595233b24adb9ef6c38a8da0050f
limda00 <dylim7715@gmail.com>
2024-06-20 16:36:17 +0900

    add :: 리뷰 내용 더미 값 추가

---
+++ src/app/dummy/reviewValue.ts
@@ -0,0 +1,26 @@
+export const reviewValue = [
+    {
+        userName: "임다영",
+        score: 4,
+        contents: "오면 없던 종교도 믿게 됩니다.",
+        image: [
+            "https://blog.kakaocdn.net/dn/yyNcR/btqzklYVznB/OKDXk6MownzwNWjZwTsJ00/img.jpg"
+        ]
+    },
+    {
+        userName: "김어진",
+        score: 2,
+        contents: "여긴 일반고가 아니고, 입학하고서 2년 반 뒤까지 취업해야하는 학교입니다. 물론 선생님에게도 잘 보이면 좋지만, 회사에서 님 생기부를 볼까요. 일반 과목 열심히 배워서 일반 과목으로 취업할 거 아니면 선배 잡아서 전공 배우세요. 그리고 저희 학교 개발하러 오는 곳입니다. 디자인을 전공으로 하려면 디자인 관련 고등학교로 가세요. 너무 좁아서 디자인 조금만 잘해도 칭찬받는 곳입니다. 밖으로 나가서 다른 최소 2,3년차 디자이너랑 경쟁해서 취업 될 것 같고, 선배 없어도 취업할 것 같으면 와서 해도 돼요.",
+        image: []
+    },
+    {
+        userName: "김승원",
+        score: 4,
+        contents: "씹덕 많아요",
+        image: [
+            "https://i.namu.wiki/i/x1Pun4wIItn-ipUfrrlbr2ccx5zu1up-rzlYDycXMaqldKhMXElx-wt693x9dXxles_PrrQcSYk2dQN9M9t6HQ.webp",
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSjcLq57YozaNaUebYnYenCe4kg12HyQptlJRWwutbN-ny3bTs5jCsjuEfAWJrHE78K-cI&usqp=CAU",
+            "https://i.ytimg.com/vi/gvUPCBbQkGY/hqdefault.jpg"
+        ]
+    }
+];
