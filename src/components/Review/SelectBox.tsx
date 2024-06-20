f2eaae8b955625839deb952b46cf93a91c478a06
limda00 <dylim7715@gmail.com>
2024-06-20 16:48:10 +0900

    add :: 리뷰 컴포넌트 추가

---
+++ src/components/Review/SelectBox.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import styled from "styled-components/native";
+import {color} from "../../styles/color"
+import {Font} from "../../styles/font"
+import {Arrow} from "../../assets/Arrow"
+
+interface PropsType {
+    text?: string
+}
+
+export default function SelectBox({text}: PropsType) {
+    return (
+        <Container>
+            <Font text={text} kind="medium14" color="gray800" />
+            <Arrow size={14} color={`${color.gray400}`} rotate="bottom"/>
+        </Container>
+    )
+}
+
+const Container = styled.View`
+padding: 10px 12px;
+gap: 8px;
+flex-direction: row;
+justify-content: space-between;
+background-color: ${color.gray100};
+border-radius: 6px;
+`
