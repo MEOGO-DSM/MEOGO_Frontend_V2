4ebf825f3208c0197e6dfd81fe23f553295cb5c6
limda00 <dylim7715@gmail.com>
2024-06-22 16:10:10 +0900

    add :: 리뷰 작성 별점 기능 구현

--- src/app/review/ReviewWrite.tsx
+++ src/app/review/ReviewWrite.tsx
@@ -32,7 +32,7 @@ export default function ReviewWrite() {
             />
             <Container>
                 <ScoreWrap>
-                    {/* <StarRating /> */}
+                    <StarRating />
                 </ScoreWrap>
                 <ContentBox>
                     <WriteWrap>
--- src/components/Review/StarRating.tsx
+++ src/components/Review/StarRating.tsx
@@ -1,95 +1,31 @@
-import React, { useState, useMemo, useCallback } from "react";
+import React, { useState } from "react";
 import styled from "styled-components/native";
-import { TransparentStarGroup } from "../../assets/TransparentStarGroup";
-import Animated, { useSharedValue, useAnimatedStyle, useDerivedValue, interpolate, Extrapolate, withTiming } from "react-native-reanimated";
-import { PanResponder } from "react-native";
+import { Star } from "../../assets";
 
-function StarRating() {
-    const [rootViewPosX, setRootViewPosX] = useState(0);
-    const [starRating, setStarRating] = useState(0)
-    const [starRatingImageWidth, setStarRatingImageWidth] = useState(0);
-    const step = useMemo(() => starRatingImageWidth * 0.1, [starRatingImageWidth])
-    const panX = useSharedValue(0)
+export default function StarRating() {
 
-    const starRatingWidth = useDerivedValue(() => {
-        return interpolate(panX.value, [0, starRatingImageWidth], [0, starRatingImageWidth], Extrapolate.CLAMP);
-    }, [starRatingImageWidth]);
+    const [stars, setStars] = useState([false, false, false, false, false]);
 
-    const animatedStyle = useAnimatedStyle(() => {
-        return {
-            width: starRatingWidth.value
-        };
-    }, []);
-
-    const panResponder = useMemo(() => PanResponder.create({
-        onStartShouldSetPanResponder: () => true,
-        onMoveShouldSetPanResponder: () => false,
-        onPanResponderTerminationRequest: () => false,
-        onPanResponderGrant: (event, gestureState) => {
-            panX.value = gestureState.x0 + gestureState.dx - rootViewPosX;
-        },
-        onPanResponderMove: (event, gestureState) => {
-            panX.value = gestureState.x0 + gestureState.dx - rootViewPosX;
-        },
-        onPanResponderRelease: (event, gestureState) => {
-            const childValue = Math.ceil(starRatingWidth.value / step)
-            panX.value= withTiming(childValue* step, {
-                duration: 100
-            })
-            setStarRating(childValue/2)
-        },
-        onPanResponderTerminate: (event, gestureState) => {
-            const childValue = Math.ceil(starRatingWidth.value / step) 
-            panX.value= withTiming(childValue* step, {
-                duration: 100
-            })
-            setStarRating(childValue/2)
-        },
-        onShouldBlockNativeResponder: (evt, gestureState) => {return false}
-    }), [rootViewPosX, starRatingImageWidth]);
-
-    const rootContainerOnLayout = useCallback((e) => {
-        const { x } = e.nativeEvent.layout;
-        setRootViewPosX(x);
-    }, []);
-
-    const starRatingImageOnLayout = useCallback((e) => {
-        const { width } = e.nativeEvent.layout;
-        setStarRatingImageWidth(width);
-    }, []);
+    const handleStarPress = (index : number) => {
+        const updatedStars = stars.map((star, idx) => idx <= index ? true : false);
+        setStars(updatedStars);
+    };
 
     return (
-        <Container onLayout={rootContainerOnLayout}>
-            <StarRatingText>
-                <StarBackground
-                    style={animatedStyle}
-                    pointerEvents="none"
-                >
-                    <Content onLayout={starRatingImageOnLayout} {...panResponder.panHandlers}>
-                        <TransparentStarGroup />
-                    </Content>
-                </StarBackground>
-            </StarRatingText>
+        <Container>
+            {stars.map((filled, index) => (
+                <Star
+                    key={index}
+                    size={42}
+                    full={filled}
+                    onPress={() => handleStarPress(index)}
+                />
+            ))}
         </Container>
-    );
+    )
 }
 
-export default StarRating;
-
 const Container = styled.View`
-align-items: center;
-`;
-
-const StarRatingText = styled.View`
 flex-direction: row;
-position: relative;
-`;
-
-const StarBackground = styled(Animated.View)`
-position: absolute;
-height: 100%;
-`;
-
-const Content = styled.View`
-flex-direction: row;
+gap: 6px;
 `
