5e64deac7857b276fc267f0c3dafae1a205d40fb
ojinikim <ojinikim@gmail.com>
2024-06-23 23:30:09 +0900

    fix :: 없는 svg 불러오기 제거

--- App.tsx
+++ App.tsx
@@ -7,8 +7,8 @@
 
 import React from 'react';
 import Login from './src/app/login/page';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Main from './src/app/main/page';
 import IdAndPassword from './src/app/signup/IdAndPassword';
 import Community from './src/app/community/page';
@@ -19,10 +19,10 @@ import School2 from './src/app/signup/School2';
 import Post from './src/app/community/Post';
 import EditPost from './src/app/community/EditPost';
 import AddTag from './src/app/community/AddTag';
-import Review from "./src/app/review/page"
-import ReviewWrite from "./src/app/review/ReviewWrite"
-import Search from "./src/app/search/page"
-import KeywordReview from "./src/app/review/KeywordReview"
+import Review from './src/app/review/page';
+import ReviewWrite from './src/app/review/ReviewWrite';
+// import Search from "./src/app/search/page"
+import KeywordReview from './src/app/review/KeywordReview';
 
 function App(): React.JSX.Element {
   const Stack = createNativeStackNavigator();
@@ -32,48 +32,48 @@ function App(): React.JSX.Element {
         <Stack.Screen
           name="Login"
           component={Login}
-          options={{ headerShown: false }}
+          options={{headerShown: false}}
         />
         <Stack.Screen
           name="IdAndPassword"
           component={IdAndPassword}
-          options={{ headerShown: false }}
+          options={{headerShown: false}}
         />
         <Stack.Screen
           name="Name"
           component={Name}
-          options={{ headerShown: false }}
+          options={{headerShown: false}}
         />
         <Stack.Screen
           name="School"
           component={School}
-          options={{ headerShown: false }}
+          options={{headerShown: false}}
         />
         <Stack.Screen
           name="FindSchool"
           component={FindSchool}
-          options={{ headerShown: false }}
+          options={{headerShown: false}}
         />
         <Stack.Screen
           name="School2"
           component={School2}
-          options={{ headerShown: false }}
+          options={{headerShown: false}}
         />
         <Stack.Screen
           name="Main"
           component={Main}
-          options={{ headerShown: false }}
+          options={{headerShown: false}}
         />
 
         <Stack.Screen
           name="Community"
           component={Community}
-          options={{ headerShown: false }}
+          options={{headerShown: false}}
         />
         <Stack.Screen
           name="Post"
           component={Post}
-          options={{ headerShown: false }}
+          options={{headerShown: false}}
         />
         <Stack.Screen
           name="EditPost"
@@ -83,27 +83,27 @@ function App(): React.JSX.Element {
         <Stack.Screen
           name="AddTag"
           component={AddTag}
-          options={{ headerShown: false }}
+          options={{headerShown: false}}
         />
         <Stack.Screen
           name="Review"
           component={Review}
-          options={{ headerShown: false }}
+          options={{headerShown: false}}
         />
         <Stack.Screen
           name="ReviewWrite"
           component={ReviewWrite}
-          options={{ headerShown: false }}
+          options={{headerShown: false}}
         />
+        {/* <Stack.Screen
+          name="Search"
+          component={Search}
+          options={{headerShown: false}}
+        /> */}
         <Stack.Screen
-        name="Search"
-        component={Search}
-        options={{headerShown: false}}
-        />
-        <Stack.Screen
-        name="KeywordReview"
-        component={KeywordReview}
-        options={{headerShown: false}}
+          name="KeywordReview"
+          component={KeywordReview}
+          options={{headerShown: false}}
         />
       </Stack.Navigator>
     </NavigationContainer>
