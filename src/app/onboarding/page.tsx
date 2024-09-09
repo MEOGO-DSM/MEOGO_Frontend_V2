ab91ffce701313e2f62be0c5e45c698ad54be766
ojinikim <ojinikim@gmail.com>
2024-09-09 14:59:06 +0900

    add :: onboarding page

---
+++ src/app/onboarding/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components/native';
+
+function Onboarding() {
+  return <Container></Container>;
+}
+
+export default Onboarding;
+
+const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
