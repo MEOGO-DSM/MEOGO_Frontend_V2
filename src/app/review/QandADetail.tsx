9f5900656f3a96bbb1be643f0ebc41f724434514
limda00 <dylim7715@gmail.com>
2024-09-15 21:18:35 +0900

    add :: Q&A 질문 답변 페이지 퍼블리싱

---
+++ src/app/review/QandADetail.tsx
@@ -0,0 +1,121 @@
+import { TopBar } from "../../components";
+import { Arrow, Setting } from '../../assets'
+import styled from "styled-components/native";
+import { Font, color } from "../../styles";
+import { TextInput } from "react-native";
+
+export default function QandADetail() {
+  return (
+    <Container>
+      <TopBar
+        text="대덕소프트웨어마이스터고"
+        leftIcon={<Arrow onPress={() => { }} />}
+      />
+
+      <QuestionWrap>
+        <InfoAndContent>
+          <InfoWrap>
+            <UserIdAndTime>
+              <Font text="limda" kind="semi14" />
+              <Font text="02.19 23:36" kind="medium12" color="gray400" />
+            </UserIdAndTime>
+            <SettingIcon>
+              <Setting color={color.gray500} rotate="horizontal" />
+            </SettingIcon>
+          </InfoWrap>
+          <Content>
+            <Font text="Q. " kind="semi18" color="amber700" />
+            <Font text="기숙사에서 몇명이 함께 방을 쓰나요? 그리고 룸메는 한번 정해지면 그대로 쭉 가는건가요? 그리고 룸메는 한번 정해지면 그대로 쭉 가는건가요?" kind="regular18" numberOfLines={3} />
+          </Content>
+        </InfoAndContent>
+        <QuestionTypeWrap>
+          <Font text="학교생활질문" kind="medium14" color="gray600" />
+          <Arrow size={18} color={color.gray600} rotate="right" />
+        </QuestionTypeWrap>
+      </QuestionWrap>
+
+      <AnswerSortWrap>
+        <Font text="2개의 답변" kind="semi16" />
+        <SortWrap>
+          <Font text="추천순" kind="medium14" color="gray500" />
+          <Arrow size={16} color={color.gray500} rotate="bottom" />
+        </SortWrap>
+      </AnswerSortWrap>
+
+      <CommentInput>
+        <TextInput placeholder="댓글을 입력해주세요" />
+      </CommentInput>
+    </Container>
+  )
+}
+
+const Container = styled.View`
+flex: 1;
+padding: 56px 0 24px;
+gap: 4px;
+background-color: ${color.gray100};
+`
+
+const QuestionWrap = styled.View`
+display: flex;
+gap: 24px;
+padding: 16px 20px;
+background-color: ${color.white};
+`
+
+const InfoAndContent = styled.View`
+display: flex;
+gap: 12px;
+`
+
+const InfoWrap = styled.View`
+display: flex;
+flex-direction: row;
+justify-content: space-between;
+padding-bottom: 8px;
+border-bottom-width: 1px;
+border-bottom-color: ${color.gray100};
+`
+
+const UserIdAndTime = styled.View`
+display: flex;
+flex-direction: row;
+align-items: center;
+gap: 12px;
+`
+
+const Content = styled.View`
+width: 100%;
+display: flex;
+gap: 4px;
+`
+
+const QuestionTypeWrap = styled.View`
+display: flex;
+flex-direction: row;
+align-items: center;
+gap: 4px;
+padding: 4px 0;
+`
+
+const AnswerSortWrap = styled.View`
+display: flex;
+flex-direction: row;
+justify-content: space-between;
+align-items: center;
+padding: 12px 20px;
+background-color: ${color.white};
+`
+
+const SortWrap = styled.View`
+display: flex;
+flex-direction: row;
+gap: 4px;
+align-items: center;
+`
+
+const CommentInput = styled.View``
+
+const SettingIcon = styled.View`
+  padding: 4px;
+`
