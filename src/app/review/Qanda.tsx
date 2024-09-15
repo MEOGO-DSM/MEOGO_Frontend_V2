fc1e89df52413eecb2605bdd50942fcbd9a00075
limda00 <dylim7715@gmail.com>
2024-09-15 13:42:57 +0900

    add :: QandA 페이지 퍼블리싱

--- src/app/review/Qanda.tsx
+++ src/app/review/Qanda.tsx
@@ -1,10 +1,46 @@
 import React from 'react';
-import {Text} from 'react-native-svg';
+import styled from 'styled-components/native';
+import { color } from '../../styles';
+import { Filter } from '../../assets';
+import { NewMarkTag } from '../../components/Review/NewMarkTag';
+import { QuestionBox } from '../../components/Review/QuestionBox';
 
 export default function QandA() {
+  const tagList = ["학교생활질문", "그외", "입학관련질문"]
   return (
-    <>
-      <Text>여기는 Q&A 페이지</Text>
-    </>
+    <Container>
+      <FilterWrap
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={{ alignItems: 'center', paddingHorizontal: 16 }}
+      >
+        <Filter color={color.gray500} />
+        <TagWrap>
+          {tagList.map((value, index) => (
+            <NewMarkTag key={index} text={value} mark={true} />
+          ))}
+        </TagWrap>
+      </FilterWrap>
+      <QuestionBox />
+      <QuestionBox />
+    </Container>
   );
 }
+
+const Container = styled.View`
+  width: 100%;
+  background-color: ${color.white};
+  padding: 24px 0;
+  gap: 16px;
+`
+
+const FilterWrap = styled.ScrollView`
+flex-direction: row;
+padding: 8px 0;
+`
+
+const TagWrap = styled.View`
+display: flex;
+flex-direction: row;
+gap: 8px;
+`
