c1b3450e2fb5533e40b5fe73393445cc0847572b
limda00 <dylim7715@gmail.com>
2024-09-23 08:56:37 +0900

    add :: Q&A 태그 선택 추가

--- src/components/Review/NewMarkTag.tsx
+++ src/components/Review/NewMarkTag.tsx
@@ -1,41 +1,46 @@
-import { Font } from "../../styles/font"
+import { Font } from "../../styles/font";
 import { color } from "../../styles/color";
 import styled from "styled-components/native";
+import { TouchableOpacity } from "react-native";
 
 interface PropsType {
   text?: string;
   mark?: boolean;
+  selected?: boolean;
+  onPress?: () => void
 }
 
-export const NewMarkTag = ({ text, mark }: PropsType) => {
+export const NewMarkTag = ({ text, mark, selected = false, onPress }: PropsType) => {
   return (
-    <Container>
-      <Font text={text} />
-      {
-        mark ?
-          (<NewMark>
+    <TouchableOpacity onPress={onPress}>
+      <Container selected={selected}>
+        <Font text={text} />
+        {mark && (
+          <NewMark>
             <Font text="N" kind="semi14" color="amber600" />
-          </NewMark>) : (<></>)
-      }
-    </Container>
-  )
-}
+          </NewMark>
+        )}
+      </Container>
+    </TouchableOpacity>
+  );
+};
 
-const Container = styled.View`
-display: flex;
-flex-direction: row;
-padding: 8px 10px;
-gap: 10px;
-border-radius: 6px;
-border: 1px solid ${color.gray200};
-`
+const Container = styled.View<{ selected: boolean }>`
+  display: flex;
+  flex-direction: row;
+  padding: 8px 10px;
+  gap: 10px;
+  border-radius: 6px;
+  border: 1px solid ${({ selected }) => (selected ? color.amber300 : color.gray200)};
+  background-color: ${({ selected }) => (selected ? color.amber50 : color.white)};
+`;
 
 const NewMark = styled.View`
-width: 24px;
-height: 24px;
-display: flex;
-align-items: center;
-padding: 4px;
-border-radius: 100px;
-background-color: ${color.amber50};
-`
+  width: 24px;
+  height: 24px;
+  display: flex;
+  align-items: center;
+  padding: 4px;
+  border-radius: 100px;
+  background-color: ${color.amber50};
+`;
