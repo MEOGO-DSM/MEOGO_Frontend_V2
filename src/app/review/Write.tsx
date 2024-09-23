36134874ee06d6c19e2c6547a05418a5e22f93df
limda00 <dylim7715@gmail.com>
2024-09-23 23:35:16 +0900

    edit :: 파일 분리 및 리팩토링

--- src/app/review/Write.tsx
+++ src/app/review/Write.tsx
@@ -8,78 +8,23 @@ import { useNavigation } from '@react-navigation/native';
 import { color } from '../../styles/color';
 import AddImgContent from '../../components/Review/AddImgContent';
 import { Star } from '../../assets';
-import { ImagePickerResponse } from 'react-native-image-picker';
-import { RequestStoragePermission } from '../../utils/RequestStoragePermission';
-import * as ImagePicker from 'react-native-image-picker';
 import { StackNavigationProp } from '@react-navigation/stack';
-import { useSelector, useDispatch } from "react-redux";
-import { add } from "../../utils/store/modules/imageAddRemove"
-import { RootState } from '../../utils/store/store';
+import useImagePicker from '../../utils/imageUpload'
 
 export default function Write() {
-  const navigation = useNavigation<StackNavigationProp<any>>()
-  const dispatch = useDispatch()
-  const image = useSelector((state: RootState) => state.imageAddRemove.image)
+  const { onSelectImage } = useImagePicker();
 
+  const navigation = useNavigation<StackNavigationProp<any>>();
+  
   const [rating, setRating] = useState<number>(0);
   const [hoverRating, setHoverRating] = useState<number>(0);
   const [contentValue, setContentValue] = useState<string>('');
-  const [limit, setLimit] = useState<number>(0)
+  const [limit, setLimit] = useState<number>(0);
 
   const handleChangeInput = (text: string) => {
     setContentValue(text);
-    setLimit(text.length)
-  }
-
-  const [response, setResponse] = useState<ImagePickerResponse | null>(null);
-  const [isPickingImage, setPickingImage] = useState<boolean>(false);
-
-  const onSelectImage = async () => {
-    // const hasPermission = await RequestStoragePermission()
-    const hasPermission = true
-
-    if (!hasPermission) {
-      console.log('이미지를 선택하려면 저장 권한이 필요합니다')
-      return;
-    }
-    if (isPickingImage) return;
-    setPickingImage(true);
-
-    ImagePicker.launchImageLibrary(
-      {
-        mediaType: 'photo',
-        maxWidth: 100,
-        maxHeight: 100,
-      }, (response) => {
-        console.log(response)
-        setPickingImage(false);
-
-        if (response.didCancel) {
-          console.log('이미지 선택을 취소했습니다.')
-          return
-        }
-        else if (response.errorCode) {
-          console.log('이미지 에러' + response.errorCode)
-          return
-        }
-        setResponse(response);
-
-        if (response.assets && response.assets.length > 0) {
-          const newImage = response.assets[0].uri;
-
-          if (newImage && image.length < 4 && !image.includes(newImage)) {
-            dispatch(add(newImage))
-            console.log("newImage:", newImage);
-            console.log("current images:", image);
-          } else {
-            console.log('더 이상 이미지를 추가할 수 없거나 이미 존재합니다')
-          }
-
-        } else {
-          console.log('이미지를 선택하지 않았습니다');
-        }
-      })
-  }
+    setLimit(text.length);
+  };
 
   return (
     <>
@@ -99,9 +44,7 @@ export default function Write() {
       <Container>
         <ScoreWrap>
           {[1, 2, 3, 4, 5].map((index) => (
-            <TouchableOpacity
-              key={index}
-            >
+            <TouchableOpacity key={index}>
               <Star
                 size={42}
                 full={(hoverRating >= index) || (!hoverRating && rating >= index)}
@@ -120,26 +63,24 @@ export default function Write() {
               placeholderTextColor={color.gray300}
               onChangeText={handleChangeInput}
               value={contentValue}
-            >
-            </MainTextWrap>
+            />
           </WriteWrap>
           <LimitText>
-            <Font text={`${limit}/300 자`} kind="medium14" color={`${limit > 300 ? "red" : "gray400"}`} />
+            <Font text={`${limit}/300 자`} kind="medium14" color={limit > 300 ? "red" : "gray400"} />
           </LimitText>
-
           <ImgWrap>
             <Font text="이미지" kind="semi20" />
-            <UploadWrap contentContainerStyle={{ columnGap: 8 }} horizontal={true} >
+            <UploadWrap contentContainerStyle={{ columnGap: 8 }} horizontal>
               <ImgUploadBox onPress={() => onSelectImage()}>
                 <Media color="gray300" />
               </ImgUploadBox>
-              {image && <AddImgContent />}
+              <AddImgContent />
             </UploadWrap>
           </ImgWrap>
         </ContentBox>
       </Container>
     </>
-  )
+  );
 }
 
 const Container = styled.View`
@@ -150,7 +91,6 @@ const Container = styled.View`
 
 const ScoreWrap = styled.View`
   padding: 20px;
-  display: flex;
   flex-direction: row;
   justify-content: center;
   align-items: center;
@@ -185,7 +125,6 @@ const ImgWrap = styled.View`
 `;
 
 const UploadWrap = styled.ScrollView`
-  display: flex;
   flex-direction: row;
   gap: 8px;
 `;
