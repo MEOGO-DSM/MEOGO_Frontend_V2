ff5ad6ab09ee126b3921efe8a0209b0d0100e1e8
limda00 <dylim7715@gmail.com>
2024-10-08 08:21:57 +0900

    edit :: 이미지 배열 이름 수정

--- src/app/review/photo/page.tsx
+++ src/app/review/photo/page.tsx
@@ -1,70 +1,68 @@
 import React from "react";
 import styled from "styled-components/native";
-import { Font } from "../../../styles/font"
+import { Font } from "../../../styles/font";
 import { color } from "../../../styles/color";
-import { photo } from "../../dummy/photo"
+import { photo } from "../../dummy/photo";
 import { TouchableOpacity } from "react-native";
 import { useDispatch } from "react-redux";
-import { show, isShow, imageArray } from "../../../utils/store/modules/appearPhoto"
+import { show, isShow, imageArray, currentIndex } from "../../../utils/store/modules/appearPhoto";
 
 export default function Photo() {
-    const dispatch = useDispatch()
-    const allPhotoLinks = photo.flatMap(item => item.image);
+    const dispatch = useDispatch();
 
-    const handleImageClick = (image: string) => {
-        dispatch(show(image))
-        dispatch(isShow(true))
-        dispatch(imageArray(allPhotoLinks))
-    }
+    const handleImageClick = (image: string, yearIndex: number, index: number) => {
+        dispatch(show(image));
+        dispatch(isShow(true));
+        dispatch(imageArray(photo));
+        dispatch(currentIndex(index, yearIndex));
+    };
 
     return (
-        <>
-            <Container>
-                {photo.map((value, yearIndex) => (
-                    <Content>
-                        <YearWrap>
-                            <Font text={`${value.year}`} kind="medium20" />
-                        </YearWrap>
-                        <ImgContentWrap>
-                            {value.image.map((image, index) => (
-                                <TouchableOpacity
-                                    key={index}
-                                    onPress={() => handleImageClick(image, index)}
-                                >
-                                    <Img source={{ uri: image }} />
-                                </TouchableOpacity>
-                            ))}
-                        </ImgContentWrap>
-                    </Content>
-                ))}
-            </Container>
-        </>
-    )
+        <Container>
+            {photo.map((value, yearIndex) => (
+                <Content key={yearIndex}>
+                    <YearWrap>
+                        <Font text={`${value.year}`} kind="medium20" />
+                    </YearWrap>
+                    <ImgContentWrap>
+                        {value.image.map((image, index) => (
+                            <TouchableOpacity
+                                key={index}
+                                onPress={() => handleImageClick(image, yearIndex, index)}
+                            >
+                                <Img source={{ uri: image }} />
+                            </TouchableOpacity>
+                        ))}
+                    </ImgContentWrap>
+                </Content>
+            ))}
+        </Container>
+    );
 }
 
 const Container = styled.View`
-width: 100%;
-background-color: ${color.white};
-padding: 24px 20px;
-gap: 44px;
-`
+    width: 100%;
+    background-color: ${color.white};
+    padding: 24px 20px;
+    gap: 44px;
+`;
 
 const Content = styled.View`
-gap: 16px;
-`
+    gap: 16px;
+`;
 
 const YearWrap = styled.View`
-padding: 8px 0px;
-border-bottom-width: 1px;
-border-bottom-color: ${color.gray100};
-`
+    padding: 8px 0px;
+    border-bottom-width: 1px;
+    border-bottom-color: ${color.gray100};
+`;
 
 const ImgContentWrap = styled.View`
-flex-direction: row;
-flex-wrap: wrap;
-`
+    flex-direction: row;
+    flex-wrap: wrap;
+`;
 
 const Img = styled.Image`
-width: 87px;
-height: 87px;
-`
+    width: 87px;
+    height: 87px;
+`;
--- src/utils/store/modules/appearPhoto.ts
+++ src/utils/store/modules/appearPhoto.ts
@@ -3,12 +3,21 @@ const IS_SHOW = 'IS_SHOW' as const;
 const IMAGE_ARRAY = 'IMAGE_ARRAY' as const;
 const NEXT_IMAGE = 'NEXT_IMAGE' as const;
 const PREV_IMAGE = 'PREV_IMAGE' as const;
+const CURRENT_INDEX = 'CURRENT_INDEX' as const;
 
 export const show = (image: string) => ({ type: CURRENT_SHOW, image });
 export const isShow = (click: boolean) => ({ type: IS_SHOW, click });
-export const imageArray = (image: Array<string>) => ({ type: IMAGE_ARRAY, image });
+export const imageArray = (image: Array<{ year: number, image: string[] }>) => ({
+  type: IMAGE_ARRAY,
+  image
+});
 export const nextImage = () => ({ type: NEXT_IMAGE });
 export const prevImage = () => ({ type: PREV_IMAGE });
+export const currentIndex = (index: number, yearIndex: number) => ({
+  type: CURRENT_INDEX,
+  index,
+  yearIndex
+});
 
 interface CurrentShowAction {
   type: typeof CURRENT_SHOW;
@@ -22,7 +31,7 @@ interface IsShowAction {
 
 interface ImageAction {
   type: typeof IMAGE_ARRAY;
-  image: Array<string>;
+  image: Array<{ year: number, image: string[] }>;
 }
 
 interface NextImageAction {
@@ -33,14 +42,21 @@ interface PrevImageAction {
   type: typeof PREV_IMAGE;
 }
 
+interface CurrentIndexAction {
+  type: typeof CURRENT_INDEX;
+  index: number;
+  yearIndex: number;
+}
+
 const initialState = {
   currentShowImageIndex: 0,
   isAppearImage: false,
-  photo: [],
+  photo: [] as Array<{ year: number, image: string[] }>,
   currentShowImage: '',
+  currentYearIndex: 0,
 };
 
-type Action = CurrentShowAction | IsShowAction | ImageAction | NextImageAction | PrevImageAction;
+type Action = CurrentShowAction | IsShowAction | ImageAction | NextImageAction | PrevImageAction | CurrentIndexAction;
 
 const appearImage = (state = initialState, action: Action) => {
   switch (action.type) {
@@ -52,17 +68,63 @@ const appearImage = (state = initialState, action: Action) => {
       return {
         ...state,
         photo: action.image,
-        currentShowImage: action.image[0],
+        currentShowImage: action.image[0].image[0],
         currentShowImageIndex: 0
       };
     }
     case NEXT_IMAGE: {
-      const nextIndex = (state.currentShowImageIndex + 1) % state.photo.length;
-      return { ...state, currentShowImageIndex: nextIndex, currentShowImage: state.photo[nextIndex] };
+      const { currentYearIndex, currentShowImageIndex } = state;
+      const currentYear = state.photo[currentYearIndex];
+      const nextIndex = currentShowImageIndex + 1;
+      
+      if (nextIndex < currentYear.image.length) {
+        return { 
+          ...state, 
+          currentShowImageIndex: nextIndex, 
+          currentShowImage: currentYear.image[nextIndex] 
+        };
+      } else if (currentYearIndex < state.photo.length - 1) {
+        const nextYearIndex = currentYearIndex + 1;
+        return { 
+          ...state, 
+          currentShowImageIndex: 0,
+          currentShowImage: state.photo[nextYearIndex].image[0],
+          currentYearIndex: nextYearIndex
+        };
+      }
+      return state;
     }
     case PREV_IMAGE: {
-      const prevIndex = (state.currentShowImageIndex - 1 + state.photo.length) % state.photo.length;
-      return { ...state, currentShowImageIndex: prevIndex, currentShowImage: state.photo[prevIndex] };
+      const { currentYearIndex, currentShowImageIndex } = state;
+      const currentYear = state.photo[currentYearIndex];
+      const prevIndex = currentShowImageIndex - 1;
+      
+      if (prevIndex >= 0) {
+        return { 
+          ...state, 
+          currentShowImageIndex: prevIndex, 
+          currentShowImage: currentYear.image[prevIndex] 
+        };
+      } else if (currentYearIndex > 0) {
+        const prevYearIndex = currentYearIndex - 1;
+        const lastYear = state.photo[prevYearIndex];
+        return { 
+          ...state, 
+          currentShowImageIndex: lastYear.image.length - 1,
+          currentShowImage: lastYear.image[lastYear.image.length - 1],
+          currentYearIndex: prevYearIndex
+        };
+      }
+      return state;
+    }
+    case CURRENT_INDEX: {
+      const selectedImage = state.photo[action.yearIndex].image[action.index];
+      return { 
+        ...state, 
+        currentShowImageIndex: action.index, 
+        currentShowImage: selectedImage,
+        currentYearIndex: action.yearIndex
+      };
     }
     default:
       return state;
