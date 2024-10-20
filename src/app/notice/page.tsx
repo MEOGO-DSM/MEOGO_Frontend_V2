3ea556186580a520e8fda2c52043a572ade83543
limda00 <dylim7715@gmail.com>
2024-10-20 14:38:29 +0900

    add :: fcm 토큰 가져오기 추가

--- src/app/notice/page.tsx
+++ src/app/notice/page.tsx
@@ -1,25 +1,51 @@
+import { useEffect } from "react"
 import { TopBar } from "../../components"
 import { Arrow } from "../../assets"
 import styled from "styled-components/native"
 import { color, Font } from "../../styles"
 import { messageList } from "../dummy/messageList"
+import messaging from "@react-native-firebase/messaging"
 
 export default function Notice() {
+
+  const getFcmToken = async () => {
+    try {
+      const fcmToken = await messaging().getToken()
+      console.log("FCM 토큰", fcmToken)
+    }
+    catch (error) {
+      console.error("FCM 토큰 가져오기 오류", error)
+    }
+  }
+
+  messaging().setBackgroundMessageHandler(async (remoteMessage) => {
+    console.log("백그라운드 메시지:", remoteMessage);
+  })
+
+  useEffect(() => {
+    const unsubscribe = messaging().onMessage(async remoteMessage => {
+      console.log("새로운 메세지 도착", JSON.stringify(remoteMessage))
+    })
+
+    getFcmToken()
+
+    return unsubscribe
+  }, [])
 
   const shouldHideMark = (date: string) => {
     const [datePart, timePart] = date.split(' ');
     const [day, month, year] = datePart.split('.').map(Number);
     const [hours, minutes] = timePart.split(':').map(Number);
-  
+
     const inputDate = new Date(2000 + year, month - 1, day, hours, minutes);
     const currentDate = new Date();
-  
+
     const timeDifference = currentDate.getTime() - inputDate.getTime();
     const twentyFourHours = 24 * 60 * 60 * 1000;
-  
+
     return timeDifference >= twentyFourHours;
   }
-  
+
   return (
     <>
       <TopBar
@@ -32,7 +58,7 @@ export default function Notice() {
             messageList.map((value, index) => (
               <MessageBox key={index}>
                 <Font text={value.text} kind="medium18" />
-                { !shouldHideMark(value.date) && <CheckIcon></CheckIcon> }
+                {!shouldHideMark(value.date) && <CheckIcon></CheckIcon>}
               </MessageBox>
             ))
           }
