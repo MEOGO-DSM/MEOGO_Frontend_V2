0948e278195124041447a18c612c7cc240caf9dd
ojinikim <ojinikim@gmail.com>
2024-10-23 23:39:44 +0900

    feat :: 로그인 api 연동

--- src/apis/user.ts
+++ src/apis/user.ts
@@ -1,4 +1,8 @@
 import {instance} from './axios';
+import {useNavigation} from '@react-navigation/native';
+import {StackNavigationProp} from '@react-navigation/stack';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {Alert} from 'react-native';
 
 interface SignupData {
   id: string;
@@ -18,35 +22,51 @@ interface ApiResponse<T> {
   data: T;
 }
 
-const auth = '/user';
+const navigation = useNavigation<StackNavigationProp<any>>();
 
 export const signupHandler = async (data: SignupData) => {
-  return await instance
-    .post(`${auth}/signup`, {
+  try {
+    const response = await instance.post(`/user/signup`, {
       account_id: data.id,
       name: data.name,
       password: data.password,
       enrolled_school: data.schoolId,
-    })
-    .then()
-    .catch(err => {
-      console.error(err);
     });
+  } catch (error) {
+    console.log(error);
+    Alert.alert('아이디 또는 비밀번호를 잘못 입력했습니다.');
+  }
 };
 
-export const loginHandler = async (data: LoginData) => {
-  return await instance
-    .post(`${auth}/login`, {account_id: data.id, password: data.password})
-    .then()
-    .catch(err => {
-      console.error(err);
+export const loginHandler = async (data: LoginData, navigation: any) => {
+  try {
+    const response = await instance.post(`/user/login`, {
+      account_id: data.id,
+      password: data.password,
     });
+
+    await AsyncStorage.setItem('AccessToken', response.data.accessToken);
+    await AsyncStorage.setItem('RefreshToken', response.data.refreshToken);
+    navigation.navigate('NavBar');
+  } catch (error) {
+    console.error(error);
+    Alert.alert('로그인 오류', '로그인이 실패했습니다.');
+  }
 };
 
 export const checkDuplicationId = async (data: any) => {
   try {
     const response = await instance.get(`/user/check`);
     return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const logoutHandler = async () => {
+  try {
+    AsyncStorage.clear();
+    navigation.navigate('Login');
   } catch (error) {
     console.error(error);
   }
